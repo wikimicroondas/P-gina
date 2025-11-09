a4509d7a30bef88f8291b8bfed2f52304c1d2ef7
wikimicroondas <211375403+wikimicroondas@users.noreply.github.com>
2025-11-10 00:05:25 +0100

    9/11/25

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Espera a que el DOM esté completamente cargado
+//Desplegar formulario
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.getElementById("toggleForm");
   const form = document.querySelector(".contactForm");
 
-  // Oculta el formulario al inicio
-  form.classList.add("hide");
+form.classList.add("hide");
 
-  // Al hacer clic en el botón, alterna la clase "hide"
-  toggleBtn.addEventListener("click", function () {
+toggleBtn.addEventListener("click", function () {
     form.classList.toggle("hide");
 
-    // Añade o quita animación de aparición
     if (!form.classList.contains("hide")) {
       form.classList.add("fade-in");
     } else {
